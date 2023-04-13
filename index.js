f45f6c682a192eb7a141664c8660189112cdfc4f
Andreas østgård <captainrax@gmail.com>
2023-04-13 14:39:51 +0200

    cors fix?

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require('cors')
 const app = express();
 
+app.options('*', cors()) // include before other routes
 app.use(cors())
 
 app.use(express.json());
